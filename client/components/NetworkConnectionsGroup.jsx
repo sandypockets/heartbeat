9baa8bad2b00d581fb31e11adc6380ce2123a731
sandypockets <zac@znacreative.com>
2023-12-29 19:20:21 -0500

    Chore: Split into group

---
+++ client/components/NetworkConnectionsGroup.jsx
@@ -0,0 +1,19 @@
+import SectionTitle from '@/components/Layout/SectionTitle';
+import SectionSubtitle from '@/components/Layout/SectionSubtitle';
+import Network from '@/components/Network';
+import Connections from '@/components/Connections';
+
+export default function NetworkConnectionsGroup({ network }) {
+  return (
+    <div className="flex flex-col gap-x-6 bg-gray-950 p-12 rounded-md">
+      <div>
+        <SectionTitle>Network</SectionTitle>
+        <SectionSubtitle>Monitor your Network usage.</SectionSubtitle>
+      </div>
+      <div className="grid grid-cols-1 xl:grid-cols-1 gap-4 lg:gap-6">
+        <Network network={network} />
+        <Connections network={network} />
+      </div>
+    </div>
+  );
+}
